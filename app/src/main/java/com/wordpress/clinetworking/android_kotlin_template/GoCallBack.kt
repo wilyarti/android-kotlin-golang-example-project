ef2eae459e6382e79432e02ed2c456e16e8131f3
wilyarti <32214044+wilyarti@users.noreply.github.com>
2018-04-30 21:51:00 +1000

    Update GoCallBack.kt

--- app/src/main/java/com/wordpress/clinetworking/android_kotlin_template/GoCallBack.kt
+++ app/src/main/java/com/wordpress/clinetworking/android_kotlin_template/GoCallBack.kt
@@ -10,9 +10,6 @@ class GoCallback(internal var context: Activity, internal var commoncontext: Com
 
     override final fun sendString(data: String) {
         try {
-            // add a thread sleep or we will get random crashes
-            // if the text being fed is too rapid
-            //Thread.sleep(100)
             var d: CharSequence
             if (!data.contains("\n")) {
                 d = data + "\n"
